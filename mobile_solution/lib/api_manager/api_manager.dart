b08d4b7c1ebfffced409aff7f8cc221868f2633d
Slava <sh-e-95@mail.ru>
2022-10-15 17:21:51 +0500

    add api manager

--- mobile_solution/lib/api_manager/api_manager.dart
+++ mobile_solution/lib/api_manager/api_manager.dart
@@ -1,1 +1,28 @@
+class ApiManager {
+  static String baseUrl = '192.168.1.120:800/api/v1/';
+  static AuthManager authEndpoint = AuthManager();
+  static WorkerManager workerEndpoint = WorkerManager();
+  static String dispatcherEndpoint = 'dispatcher';
+}
 
+class AuthManager {
+  final String endpoint = 'auth';
+  loginUser() {}
+  getUser() {}
+  getAllUser() {}
+}
+
+class WorkerManager {
+  final String endpoint = 'worker';
+  getWorker() {}
+  getOrders() {}
+  getOrderById() {}
+  createOrder() {}
+  getMachines() {}
+  getMachineById() {}
+  createMachine() {}
+}
+
+class DispatherManager {
+  getDispatcher() {}
+}
